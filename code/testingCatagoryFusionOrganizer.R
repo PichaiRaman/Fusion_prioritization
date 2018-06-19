fa81838f40f80ef36de148a2d9ca65dd92d27114
nickchim <nichchim@haverford.org>
2018-06-19 15:35:01 -0400

    Adding more data and beginning categorySorting

---
+++ code/testingCatagoryFusionOrganizer.R
@@ -0,0 +1,5 @@
+library(readr)
+library(stringr)
+
+DB_Data <- read_excel("../Fusion_prioritization/Data/ChimerDB3.0_ChimerSeq.xlsx")
+Jack_Data <-read_tsv("../Fusion_prioritization/Data/pancanfus.txt")
