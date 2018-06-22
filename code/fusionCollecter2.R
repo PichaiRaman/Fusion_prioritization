f955eb4c67570578463e88a837a37e7a8a36d66a
nickchim <nichchim@haverford.org>
2018-06-22 14:56:43 -0400

    More Data

--- code/fusionCollecter2.R
+++ code/fusionCollecter2.R
@@ -40,7 +40,7 @@ Jack_Data <-read_tsv("/home/nick/Desktop/Fusion_prioritization/Data/pancanfus.tx
 Cos_Data <-read_tsv("/home/nick/Desktop/Fusion_prioritization/Data/CosmicFusionExport.tsv")
 Fuse_Data <- read_tsv("/home/nick/Desktop/Fusion_prioritization/Data/fusion_table.csv")
 
-#Cleaning up DB_Data to get rid of weird dates
+#Cleaning up DB_Data to standarize dates
 counter <- 1
 for (gene in DB_Data$H_gene) {
   if (str_detect(gene,"\\.") && str_locate(gene,"\\.") < 4) {
@@ -77,11 +77,34 @@ for (gene in Fuse_Data$Head_gene_symbol) {
 
 Fusions <- c("Blank")
 FusionTable <- tibble(Fusions,Count=0)
+
+#Data from CHIMERDB30
 FusionTable <- addData(FusionTable,DB_Data,"DB",DB_Data$H_gene,DB_Data$T_gene)
+
 FusionTable <- FusionTable[-1,]
+
+#Data from Jackson Lab
 FusionTable <- addData(FusionTable,Jack_Data,"Jackson",Jack_Data$Gene_A,Jack_Data$Gene_B)
+
+#Data from Fusion Cancer
 FusionTable <- addData(FusionTable,Fuse_Data,"Fusion",Fuse_Data$Head_gene_symbol,Fuse_Data$Tail_gene_symbol)
+
+#Data from Cosmic
 FusionTable <- addData(FusionTable,Cos_Data,"Cosmic",Cos_Data$GeneA,Cos_Data$GeneB)
+
+FusionTable<- read_tsv("/home/nick/Desktop/Fusion_prioritization/Data/Processed/FusionList2.txt")
+ChiTA_Data <- read_excel("/home/nick/Desktop/Fusion_prioritization/Data/Maher-Nature-fusions.xls")
+TIC_Data <- read_tsv("/home/nick/Desktop/Fusion_prioritization/Data/allseqs_TICdb.txt", col_names = FALSE)
+CG_Data <- read_tsv("/home/nick/Desktop/Fusion_prioritization/Data/ConjoinG.tsv")
+
+#Data from ChiTaRS
+FusionTable <- addData(FusionTable,ChiTA_Data,"ChiTaRS",ChiTA_Data$gene1,ChiTA_Data$gene2)
+
+#Data from TICdb
+FusionTable <- addData(FusionTable,TIC_Data,"TICdb",TIC_Data$X1,TIC_Data$X2)
+
+#Data from ConjoinG
+FusionTable <- addData(FusionTable,CG_Data,"ConjoinG",CG_Data$`Parent gene 1`,CG_Data$`Parent gene 2`)
 
 View(FusionTable)
 write.table(FusionTable, "/home/nick/Desktop/Fusion_prioritization/Data/Processed/FusionList2.txt", sep="\t",row.names = FALSE)
